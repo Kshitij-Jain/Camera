e253f274f0c22be53956866e5b7446f691eed14b
Kshitij Jain <kshitij.jain007.kj@gmail.com>
2017-02-02 18:12:03 +0530

    Main Activity: Take Picture Method And Get The Thumbnail Method Added

--- app/src/main/java/com/github/kshitij_jain/camera/MainActivity.java
+++ app/src/main/java/com/github/kshitij_jain/camera/MainActivity.java
@@ -1,13 +1,44 @@
 package com.github.kshitij_jain.camera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    // Take a Photo with the Camera App
+    public void takePicture(View view) {
+        Intent intent = new Intent(ACTION_IMAGE_CAPTURE);
+        // Check if is there is any application to capture image
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Get The Thumbnail
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
