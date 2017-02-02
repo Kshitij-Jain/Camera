a87bb8d8e3825f3b18254e96bdbe5fa154bea4f1
Kshitij Jain <kshitij.jain007.kj@gmail.com>
2017-02-02 19:01:44 +0530

    Camera : Take And Save Image Methods Added

--- app/src/main/java/com/github/kshitij_jain/camera/MainActivity.java
+++ app/src/main/java/com/github/kshitij_jain/camera/MainActivity.java
@@ -2,16 +2,29 @@ package com.github.kshitij_jain.camera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
+import static android.provider.MediaStore.EXTRA_OUTPUT;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int SAVE_IMAGE_CAPTURE = 2;
+
+    private String mCurrentPhotoPath;
 
     private ImageView mImageView;
 
@@ -32,13 +45,58 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Take and Save a Photo with the Camera App
+    public void takeSavePicture(View view) {
+        Intent intent = new Intent(ACTION_IMAGE_CAPTURE);
+        // Check if is there is any application to capture image
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = saveImage();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.github.kshitij_jain.camera.fileprovider",
+                        photoFile);
+                // It will stop android to provide thumbnail
+                intent.putExtra(EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, SAVE_IMAGE_CAPTURE);
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Get The Thumbnail
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
+            // The Android Camera application encodes the photo in the return Intent delivered
+            // to onActivityResult() as a small Bitmap in the extras, under the key "data"/
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(imageBitmap);
         }
+    }
+
+    // Save Image To Public Image Directory
+    private File saveImage() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        // Files you save in the directories provided by getExternalFilesDir()
+        // or getFilesDir() are deleted when the user uninstalls your app.
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.d("Image Save Path", mCurrentPhotoPath);
+        return image;
     }
 }
